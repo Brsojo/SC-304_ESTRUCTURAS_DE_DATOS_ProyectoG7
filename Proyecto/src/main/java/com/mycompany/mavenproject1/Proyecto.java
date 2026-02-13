4c0985769a1cb94ac37b2bc7b07251df246a9929
alexpc02 <alexpc02@hotmail.com>
2026-02-13 17:34:15 -0600

    Cambio de nombre del proyecto

---
+++ Proyecto/src/main/java/com/mycompany/mavenproject1/Proyecto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author alexp
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola");
+        System.out.println("Prueba");
+        System.out.println("Prueba2");
+    }
+}
