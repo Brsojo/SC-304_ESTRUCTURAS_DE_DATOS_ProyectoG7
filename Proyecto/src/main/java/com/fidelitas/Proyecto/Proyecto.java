eb1dbe098d684583005c736534179c4ba5e2b631
Brsojo <brandomyairr@gmail.com>
2026-02-13 19:42:07 -0600

    Creación del proyecto

--- Proyecto/src/main/java/com/fidelitas/Proyecto/Proyecto.java
+++ Proyecto/src/main/java/com/fidelitas/Proyecto/Proyecto.java
@@ -2,18 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject1;
+package com.fidelitas.Proyecto;
 
 /**
  *
- * @author alexp
+ * @author brand
  */
 public class Proyecto {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hola");
-        System.out.println("Prueba");
-        System.out.println("Prueba2");
     }
 }
