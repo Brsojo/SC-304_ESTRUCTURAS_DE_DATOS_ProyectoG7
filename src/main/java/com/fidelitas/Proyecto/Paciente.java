fabedb6abad22c16a1e5e325deb6e3cdb1316d56
alexpc02 <alexpc02@hotmail.com>
2026-02-20 19:57:58 -0600

    Creación de la clase Paciente.

---
+++ src/main/java/com/fidelitas/Proyecto/Paciente.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fidelitas.Proyecto;
+
+import java.util.Date;
+
+/**
+ *
+ * @author alexp
+ */
+public class Paciente {
+    
+    private String ficha;
+    private String cedula;
+    private String nombre;
+    private String motivoSalida;
+    private Date fechaLlegada;
+    private Date fechaSalida;
+    private String tipo; // "Regular" o "Preferencial"
+
+    public Paciente(String ficha, String cedula, String nombre, String tipo) {
+        this.ficha = ficha;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.fechaLlegada = new Date();
+    }
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMotivoSalida() {
+        return motivoSalida;
+    }
+
+    public void setMotivoSalida(String motivoSalida) {
+        this.motivoSalida = motivoSalida;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(Date fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
